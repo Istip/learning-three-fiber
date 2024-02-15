e370d5234ac4a5717a7644d9ff5505960d7e2323
Pasztor Isti <isticsek@gmail.com>
2024-02-15 21:12:50 +0200

    remove wrapper div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,14 @@ import CameraWrapper from "@/components/Utils/CameraWrapper";
 export default function Home() {
   return (
     <>
-      <div>
-        <CameraWrapper preset="city">
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <mesh>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="hotpink" />
-          </mesh>
-        </CameraWrapper>
-      </div>
+      <CameraWrapper preset="city">
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </CameraWrapper>
     </>
   );
 }
